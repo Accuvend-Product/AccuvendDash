0d008725bdcb4d4ef20690751e5377df1caa4975
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-11-13 20:15:52 +0100

    requests structure

--- src/api/apiRequests.js
+++ src/api/apiRequests.js
@@ -81,3 +81,42 @@ export function useSignIn() {
 
     return signInMutation;
 }
+
+export async function getDashboardTableData(queries) {
+    const response = await fetch(BASE_URL + 'transaction', {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`, // Include authorization header if required
+        },
+        query: queries
+    });
+
+    console.log("response", response)
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch dashboard data');
+    }
+
+    return await response.json();
+}
+
+export function useDashboardTableData() {
+    const queryClient = useQueryClient();
+    const navigate = useNavigate();
+
+    const { data, mutate } = useMutation(
+        () => getDashboardTableData(),
+        {
+            onSuccess: (data) => {
+                // send data to the table
+                toast.success('Sign in successful');
+            },
+            onError: (error) => {
+                toast.error('Ops.. Error retrieving table information', error);
+            },
+        }
+    );
+
+    return data;
+}
+
