ea5aa2cd1bba79236a5d80a9f28100f965b4132d
Joshua <cutejosh2@gmail.com>
2023-11-13 09:10:25 +0100

    chore: partner dashoard settings complete ui

--- src/pages/partner/dashboard-settings.jsx
+++ src/pages/partner/dashboard-settings.jsx
@@ -4,6 +4,8 @@ import Sidebar from "./sidebar";
 import EmailIcon from "../../components/icons/email";
 import BellIcon from "../../components/icons/bell";
 import LinkIcon from "../../components/icons/link";
+import LockIcon from "../../components/icons/lock";
+import PhoneIcon from "../../components/icons/phone";
 
 const PartnerDashboardSettings = () => {
     return (
@@ -24,13 +26,13 @@ const PartnerDashboardSettings = () => {
 
                     <div className="mt-10 flex justify-center border border-gray-200 rounded-md">
                         {/* Container div */}
-                        <div className=" flex flex-col space-y-8 items-center pt-4 w-96">
+                        <div className="flex flex-col space-y-8 items-center pt-4 pb-14 w-1/2">
                             {/* Email Settings */}
                             <div className="flex items-center justify-between w-full">
                                 <div className="flex items-center gap-4">
                                     <EmailIcon />
                                     <div className="flex flex-col">
-                                        <p>Email Settings</p>
+                                        <p className="font-bold">Email Settings</p>
                                         <div className="flex items-center gap-2">
                                             <p className="text-sm">zenithbankplc@gmail.com</p>
                                             <span className="text-secondary bg-[#E7F6EC] px-2 py-0.5 rounded-full text-xs">Verified</span>
@@ -45,22 +47,61 @@ const PartnerDashboardSettings = () => {
                                 <div className="flex items-center gap-4">
                                     <BellIcon />
                                     <div className="flex flex-col">
-                                        <p>Notifications</p>
+                                        <p className="font-bold">Notifications</p>
                                         <p className="text-sm">Choose what we get in touch about</p>
                                     </div>
                                 </div>
                                 <ChevronDown className="h-4 w-4 ml-auto" />
                             </div>
-                            
+
                             {/* Connected account */}
                             <div className="flex items-center justify-between w-full">
                                 <div className="flex items-center gap-4">
                                     <LinkIcon />
                                     <div className="flex flex-col">
-                                        <p>Connected ACcounts</p>
+                                        <p className="text-gray-300 font-bold">Connected Accounts</p>
                                         <div className="flex items-center gap-2">
-                                            <p className="text-sm">Integrations connected to your Rayna account</p>
-                                            <span className="text-[#954A00] bg-[#FFCA96] px-2 py-0.5 rounded-full text-xs">Coming Soon</span>
+                                            <p className="text-sm text-gray-300">Integrations connected to your Rayna account</p>
+                                            <span className="text-[#954A00] bg-[#FFCA96] px-2 py-0.5 rounded-full text-xs text-center">Coming Soon</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <ChevronDown className="h-4 w-4 ml-auto" />
+                            </div>
+
+                            {/* change password */}
+                            <div className="flex items-center justify-between w-full">
+                                <div className="flex items-center gap-4">
+                                    <LockIcon />
+                                    <div className="flex flex-col">
+                                        <p className="font-bold">Change Password</p>
+                                        <p className="text-sm">***************</p>
+                                    </div>
+                                </div>
+                                <ChevronDown className="h-4 w-4 ml-auto" />
+                            </div>
+                            
+                            {/* 2 step verification */}
+                            <div className="flex items-center justify-between w-full">
+                                <div className="flex items-center gap-4">
+                                    <PhoneIcon />
+                                    <div className="flex flex-col">
+                                        <p className="font-bold">2-step verification</p>
+                                        <p className="text-sm">Manage your 2-step authentication methods</p>
+                                    </div>
+                                </div>
+                                <ChevronDown className="h-4 w-4 ml-auto" />
+                            </div>
+
+                            {/* Webhooks */}
+                            <div className="flex items-center justify-between w-full">
+                                <div className="flex items-center gap-4">
+                                    <LinkIcon />
+                                    <div className="flex flex-col">
+                                        <p className="text-gray-300 font-bold">Wehooks</p>
+                                        <div className="flex items-center gap-2">
+                                            <p className="text-sm text-gray-300">You do not have any active webhooks</p>
+                                            <span className="text-[#954A00] bg-[#FFCA96] px-2 py-0.5 rounded-full text-xs text-center">Coming Soon</span>
                                         </div>
                                     </div>
                                 </div>
@@ -68,6 +109,7 @@ const PartnerDashboardSettings = () => {
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </>
