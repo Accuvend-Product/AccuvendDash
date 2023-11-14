85a37d5447bb7908113dba611d351e6c420260f1
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-11-14 09:03:01 +0100

    fixed side bar

--- src/pages/partner/sidebar.jsx
+++ src/pages/partner/sidebar.jsx
@@ -1,55 +1,77 @@
-import { Cpu, HelpCircle, LayoutDashboard, Settings, User } from "lucide-react"
-
-const links = [
-    {
-        name: "DASHBOARD",
-        icon: <LayoutDashboard className="h-5 w-5 mr-2" />,
-        active: true,
-        href: "/partner-dashboard",
-    },
-    {
-        name: "PROFILE",
-        icon: <User className="h-5 w-5 mr-2" />,
-        active: false,
-        href: "partner-dashboard/profile",
-    },
-    {
-        name: "RESOLUTION CENTER",
-        icon: <HelpCircle className="h-5 w-5 mr-2" />,
-        active: false,
-        href: "/resolution-center",
-    },
-    {
-        name: "DEV CENTER",
-        icon: <Cpu className="h-5 w-5 mr-2" />,
-        active: false,
-        href: "/partner-dashboard/devcenter",
-    },
-]
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { Cpu, HelpCircle, LayoutDashboard, Settings, User } from "lucide-react";
 
 const Sidebar = () => {
-    return (
-        <div className="min-h-screen flex">
-            <div className="w-[372px] h-full overflow-hidden border-r border-body1 flex flex-col justify-between">
-                <div className="flex flex-col pt-8 space-y-4 px-8 sm:px-10 md:px-12">
-                    {links.map((link) => (
-                        <a href={link.href} className={`flex items-center hover:text-primary ${link.active ? 'text-primary' : 'text-black'}`} key={link.name}>
-                            {link.icon}
-                            <p className="text-2xl">{link.name}</p>
-                        </a>
-                    ))}
+  const [links, setLinks] = useState([
+    {
+      name: "DASHBOARD",
+      icon: <LayoutDashboard className="h-5 w-5 mr-2" />,
+      href: "/partner-dashboard",
+      active: false,
+    },
+    {
+      name: "PROFILE",
+      icon: <User className="h-5 w-5 mr-2" />,
+      href: "/partner-dashboard/profile",
+      active: false,
+    },
+    {
+      name: "RESOLUTION CENTER",
+      icon: <HelpCircle className="h-5 w-5 mr-2" />,
+      href: "/resolution-center",
+      active: false,
+    },
+    {
+      name: "DEV CENTER",
+      icon: <Cpu className="h-5 w-5 mr-2" />,
+      href: "/partner-dashboard/devcenter",
+      active: false,
+    },
+  ]);
 
-                </div>
-                {/* horizontal separator */}
-                <div className="sm:px-10 md:px-12 pb-8">
-                    <a href="/settings" className={`flex items-center text-black hover:text-primary`}>
-                        <Settings className="mr-2 h-5 w-5" />
-                        <p className="text-2xl">SETTINGS</p>
-                    </a>
-                </div>
-            </div>
+  const location = useLocation();
+
+  useEffect(() => {
+    // Update the links based on the current location
+    setLinks((prevLinks) =>
+      prevLinks.map((link) => ({
+        ...link,
+        active: location.pathname === link.href,
+      }))
+    );
+  }, [location.pathname]);
+
+  return (
+    <div className="min-h-screen flex">
+      <div className="w-[372px] h-full overflow-hidden border-r border-body1 flex flex-col justify-between">
+        <div className="flex flex-col pt-8 space-y-4 px-8 sm:px-10 md:px-12">
+          {links.map((link) => (
+            <a
+              href={link.href}
+              className={`flex items-center hover:text-primary ${
+                link.active ? "text-primary" : "text-black"
+              }`}
+              key={link.name}
+            >
+              {link.icon}
+              <p className="text-2xl">{link.name}</p>
+            </a>
+          ))}
         </div>
-    )
-}
+        {/* horizontal separator */}
+        <div className="sm:px-10 md:px-12 pb-8">
+          <a
+            href="/settings"
+            className={`flex items-center text-black hover:text-primary`}
+          >
+            <Settings className="mr-2 h-5 w-5" />
+            <p className="text-2xl">SETTINGS</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
