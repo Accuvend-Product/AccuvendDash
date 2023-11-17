b5f5ba3f36e81e1a14d3ad953b7ab4457fcfd062
Joshua <cutejosh2@gmail.com>
2023-11-17 16:05:27 +0100

    chore: logout button n loading screens

--- src/pages/partner/dashboard.jsx
+++ src/pages/partner/dashboard.jsx
@@ -7,125 +7,159 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 const PartnerDashboard = () => {
-  const [tableData, setTableData] = useState([]);
-  const [totalTransactions, setTotalTransactions] = useState(0);
-  const [totalAmount, setTotalAmount] = useState(0);
-  const [failedTransactions, setFailedTransactions] = useState(0);
+    const [tableData, setTableData] = useState([]);
+    const [totalTransactions, setTotalTransactions] = useState(0);
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [failedTransactions, setFailedTransactions] = useState(0);
 
-  const { isLoading } = useQuery({
-    queryKey: ["transactions"],
-    queryFn: async () => {
-      const response = await axios.get(`${BASE_URL}transaction`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
+    const { isLoading } = useQuery({
+        queryKey: ["transactions"],
+        queryFn: async () => {
+            const response = await axios.get(`${BASE_URL}transaction`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+
+            const transformedData = response.data.data.transactions.map(
+                (transaction) => ({
+                    image: transaction.powerUnit?.discoLogo ? transaction.powerUnit?.discoLogo : "https://res.cloudinary.com/richiepersonaldev/image/upload/v1699947957/dpijlhj08ard76zao2uk.jpg",
+                    disco: transaction.disco ?? "TEST",
+                    "meter number": transaction.meter.meterNumber,
+                    "customer name": transaction.user.name,
+                    "transaction reference": transaction.bankRefId,
+                    "transaction date": transaction.transactionTimestamp,
+                    amount: `N${transaction.amount}`,
+                    status: transaction.status.toLowerCase(),
+                    selection: transaction.partnerId ?? "TESTID",
+                })
+            );
+
+            setTableData(transformedData);
+            return transformedData;
         },
-      });
+    });
 
-      const transformedData = response.data.data.transactions.map(
-        (transaction) => ({
-          image: transaction.powerUnit?.discoLogo ? transaction.powerUnit?.discoLogo : "https://res.cloudinary.com/richiepersonaldev/image/upload/v1699947957/dpijlhj08ard76zao2uk.jpg",
-          disco: transaction.disco ?? "TEST",
-          "meter number": transaction.meter.meterNumber,
-          "customer name": transaction.user.name,
-          "transaction reference": transaction.bankRefId,
-          "transaction date": transaction.transactionTimestamp,
-          amount: `N${transaction.amount}`,
-          status: transaction.status.toLowerCase(),
-          selection: transaction.partnerId ?? "TESTID",
-        })
-      );
+    const { isLoading: totalTransactionsLoading, } = useQuery({
+        queryKey: ["transactions", "total"],
+        queryFn: async () => {
+            const response = await axios.get(`${BASE_URL}transaction/yesterday`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
 
-      setTableData(transformedData);
-      return transformedData;
-    },
-  });
-
-  const { isLoading: totalTransactionsLoading, } = useQuery({
-    queryKey: ["transactions", "total"],
-    queryFn: async () => {
-      const response = await axios.get(`${BASE_URL}transaction/yesterday`, {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-        },
-      });
-
-      const totalTransactionCount = response.data.data.transactions.length;
+            const totalTransactionCount = response.data.data.transactions.length;
             const allTotalTransactionCount = response.data.data.totalAmount;
 
-      setTotalTransactions(totalTransactionCount);
-      setTotalAmount(allTotalTransactionCount);
+            setTotalTransactions(totalTransactionCount);
+            setTotalAmount(allTotalTransactionCount);
 
-      return totalTransactionCount;
-    },
-  });
+            return totalTransactionCount;
+        },
+    });
 
-  
-  const { isLoading: failedTransactionsLoading } = useQuery({
-    queryKey: ["transactions", "failed"],
-    queryFn: async () => {
-      const response = await axios.get(
-        `${BASE_URL}transaction/yesterday?status=failed`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
 
-      const failedTransactionCount = response.data.data.transactions.length;
-      setFailedTransactions(failedTransactionCount);
+    const { isLoading: failedTransactionsLoading } = useQuery({
+        queryKey: ["transactions", "failed"],
+        queryFn: async () => {
+            const response = await axios.get(
+                `${BASE_URL}transaction/yesterday?status=failed`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                }
+            );
 
-      return failedTransactionCount;
-    },
-  });
+            const failedTransactionCount = response.data.data.transactions.length;
+            setFailedTransactions(failedTransactionCount);
 
-  return (
-    <>
-      <Navbar />
-      <div className="flex">
-        <Sidebar />
-        <div className=" px-8 sm:px-10 md:px-12 border-b border-body1 flex-1 pb-10">
-          <div className=" ml-auto py-10">
-            {/* cards */}
-            <div className="flex space-x-12 items-center justify-end">
-              <div className="">
-                <p className="text-2xl">Total no. Transacted 24hrs</p>
-                <p className="text-[48px] text-primary font-semibold">
-                  {totalTransactionsLoading ? "Loading..." : totalTransactions}
-                </p>
-              </div>
-              <div className="">
-                <p className="text-2xl">Failed Transactions</p>
-                <p className="text-[48px] text-primary font-semibold">
-                  {failedTransactionsLoading
-                    ? "Loading..."
-                    : failedTransactions}
-                </p>
-              </div>
-              <div className="bg-primary text-white p-4">
-                <p className="text-2xl">Total Amount Transacted 24hrs</p>
-                <p className="text-[48px] font-semibold">
-                  {" "}
-                  {totalTransactionsLoading ? "Loading..." : totalAmount}
-                </p>
-              </div>
+            return failedTransactionCount;
+        },
+        staleTime: 1000 * 60 * 60,
+    });
+
+    return (
+        <>
+            <Navbar />
+            <div className="flex">
+                <Sidebar />
+                <div className=" px-8 sm:px-10 md:px-12 border-b border-body1 flex-1 pb-10">
+                    <div className=" ml-auto py-10">
+                        {/* cards */}
+                        <div className="flex space-x-12 items-center justify-end">
+                            <div className="">
+                                <p className="text-2xl">Total no. Transacted Yesterday</p>
+                                <p className="text-[48px] text-primary font-semibold">
+                                    {totalTransactionsLoading ? (<div className="flex items-center gap-2">
+                                        <div role="">
+                                            <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                            </svg>
+                                        </div>
+                                        <p className="text-primary">Loading...</p>
+                                    </div>) : totalTransactions || 0}
+                                </p>
+                            </div>
+                            <div className="">
+                                <p className="text-2xl">Failed Transactions</p>
+                                <p className="text-[48px] text-primary font-semibold">
+                                    {failedTransactionsLoading
+                                        ? (<div className="flex items-center gap-2">
+                                            <div role="">
+                                                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                                </svg>
+                                            </div>
+                                            <p className="text-primary">Loading...</p>
+                                        </div>)
+                                        : failedTransactions || 0}
+                                </p>
+                            </div>
+                            <div className="bg-primary text-white p-4">
+                                <p className="text-2xl">Total Amount Transacted 24hrs</p>
+                                <p className="text-[48px] font-semibold">
+                                    {" "}
+                                    {totalTransactionsLoading ? (<div className="inline-flex items-center">
+                                        <div className="flex items-center gap-2">
+                                            <div role="">
+                                                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                                </svg>
+                                            </div>
+                                            <p className="text-white">Loading...</p>
+                                        </div>
+                                    </div>) : totalAmount || 0}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {isLoading ? (
+                        <div className="h-1/2 w-full flex items-center justify-center">
+                            <div role="">
+                                <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                            </div>
+                        </div>
+                    ) : (
+                        tableData && (
+                            <>
+                                {/* Render your cards here */}
+                                <PartnerTransactionTable tableData={tableData} />
+                            </>
+                        )
+                    )}
+                </div>
             </div>
-          </div>
-
-          {isLoading ? (
-            <p>Loading...</p>
-          ) : (
-            tableData.length > 0 && (
-              <>
-                {/* Render your cards here */}
-                <PartnerTransactionTable tableData={tableData} />
-              </>
-            )
-          )}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default PartnerDashboard;
