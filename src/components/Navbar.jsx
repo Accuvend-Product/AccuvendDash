a4adfc2918e07a933459dbc5611152e88b224d1f
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2023-11-17 17:53:52 +0100

    Refactor logout functionality in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,81 +7,81 @@ import axios from "axios";
 import { toast } from "react-hot-toast";
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
-
 const Navbar = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleLogout = async () => {
-        try {
-            // Retrieve the authentication token from local storage
-            const authToken = localStorage.getItem("token");
+  const handleLogout = async () => {
+    try {
+      // Retrieve the authentication token from local storage
+      const authToken = localStorage.getItem("token");
 
-            // Send a POST request to the server's logout endpoint with the authentication token as data
-            const response = await axios.post(
-                `${BASE_URL}auth/logout`,
-                {
-                    token: authToken,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem("token")}`,
-                    },
-                }
-            );
-
-            // Check if the logout was successful
-            if (response.status === 200) {
-                // Clear authentication token and user email from local storage
-                localStorage.removeItem("token");
-                localStorage.removeItem("userEmail");
-
-                toast.success("Logout Successful");
-
-                // Redirect to the login page
-                navigate("/");
-            } else {
-                toast.error("Logout Failed. Please try again.");
-            }
-        } catch (error) {
-            console.log(error)
-            console.error("Error logging out:", error.message);
-            toast.error("An error occurred while logging out. Please try again.");
+      // Send a POST request to the server's logout endpoint with the authentication token as data
+      const response = await axios.post(
+        `${BASE_URL}auth/logout`,
+        {
+          token: authToken,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
-    };
-    return (
-        <div className="text-body1 border-b border-body1/80">
-            <div className="h-[121px] px-8 md:px-10 flex items-center justify-between mx-auto">
-                <a href="/dashboard" className="">
-                    <img src={Logo} className="hidden sm:block aspect-auto" />
-                    <img src={Logo} className="sm:hidden w-32 h-10 aspect-auto" />
-                </a>
-                <div className="flex items-center justify-between ml-auto space-x-10 sm:space-x-16 md:space-x-[72px] text-xl">
-                    {/* <a href="#">HELP</a> */}
-                    <a href="#" className="flex  items-center gap-2 bg-primary px-3 py-2 rounded-lg text-white"
-                        onClick={handleLogout}
-                    >
-                        <LogOutIcon />
-                        LOGOUT
-                    </a>
-                    <div className="flex items-center">
-                        <img src={ProfilePic} alt="profile picture" className="h-10 w-10" />
-                        <a href="#" className="ml-2 hidden sm:block">
-                            Admin 1
-                        </a>
-                        <ChevronDown className="h-4 w-4 ml-2 hidden sm:block" />
-                    </div>
-                </div>
-                <div className="flex sm:hidden items-center space-x-4">
-                    {/* <a href="/help">HELP</a> */}
-                    {/* <a href="#">SECURITY CENTER</a> */}
-                    <div className="flex items-center">
-                        <img src={ProfilePic} alt="profile picture" className="h-8 w-8" />
-                        <ChevronDown className="h-4 w-4 ml-2" />
-                    </div>
-                </div>
-            </div>
+      );
+
+      // Check if the logout was successful
+      if (response.status === 200) {
+        toast.success("Logout Successful");
+      } else {
+        toast.error("Logout Failed. Please try again.");
+      }
+    } catch (error) {
+      console.log(error);
+      console.error("Error logging out:", error.message);
+      toast.error("An error occurred while logging out. Please try again.");
+    } finally {
+      // Clear authentication token and user email from local storage
+      localStorage.removeItem("token");
+      localStorage.removeItem("userEmail");
+      // Redirect to the login page
+      navigate("/");
+    }
+  };
+  return (
+    <div className="text-body1 border-b border-body1/80">
+      <div className="h-[121px] px-8 md:px-10 flex items-center justify-between mx-auto">
+        <a href="/dashboard" className="">
+          <img src={Logo} className="hidden sm:block aspect-auto" />
+          <img src={Logo} className="sm:hidden w-32 h-10 aspect-auto" />
+        </a>
+        <div className="flex items-center justify-between ml-auto space-x-10 sm:space-x-16 md:space-x-[72px] text-xl">
+          {/* <a href="#">HELP</a> */}
+          <a
+            href="#"
+            className="flex  items-center gap-2 bg-primary px-3 py-2 rounded-lg text-white"
+            onClick={handleLogout}
+          >
+            <LogOutIcon />
+            LOGOUT
+          </a>
+          <div className="flex items-center">
+            <img src={ProfilePic} alt="profile picture" className="h-10 w-10" />
+            <a href="#" className="ml-2 hidden sm:block">
+              Admin 1
+            </a>
+            <ChevronDown className="h-4 w-4 ml-2 hidden sm:block" />
+          </div>
         </div>
-    );
+        <div className="flex sm:hidden items-center space-x-4">
+          {/* <a href="/help">HELP</a> */}
+          {/* <a href="#">SECURITY CENTER</a> */}
+          <div className="flex items-center">
+            <img src={ProfilePic} alt="profile picture" className="h-8 w-8" />
+            <ChevronDown className="h-4 w-4 ml-2" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
