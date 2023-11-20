7e2695a111f076ec198ac92247c1b004c2235f75
Joshua <cutejosh2@gmail.com>
2023-11-20 17:16:57 +0100

    fix: logout button

--- src/pages/partner/dashboard.jsx
+++ src/pages/partner/dashboard.jsx
@@ -89,9 +89,9 @@ const PartnerDashboard = () => {
                     <div className=" ml-auto py-10">
                         {/* cards */}
                         <div className="flex space-x-12 items-center justify-end">
-                            <div className="">
+                            <div className="bg-gray-300 hover:text-white hover:bg-primary px-4 py-2 rounded-lg hover:cursor-pointer">
                                 <p className="text-2xl">Total no. Transacted Yesterday</p>
-                                <p className="text-[48px] text-primary font-semibold">
+                                <p className="text-[48px] text-primary font-semibold hover:text-white">
                                     {totalTransactionsLoading ? (<div className="flex items-center gap-2">
                                         <div role="">
                                             <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -99,13 +99,13 @@ const PartnerDashboard = () => {
                                                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                             </svg>
                                         </div>
-                                        <p className="text-primary">Loading...</p>
+                                        <p className="text-primary hover:text-white">Loading...</p>
                                     </div>) : totalTransactions || 0}
                                 </p>
                             </div>
-                            <div className="">
+                            <div className="bg-gray-300 hover:text-white hover:bg-primary px-4 py-2 rounded-lg hover:cursor-pointer">
                                 <p className="text-2xl">Failed Transactions</p>
-                                <p className="text-[48px] text-primary font-semibold">
+                                <p className="text-[48px] text-primary font-semibold hover:text-white">
                                     {failedTransactionsLoading
                                         ? (<div className="flex items-center gap-2">
                                             <div role="">
@@ -114,12 +114,12 @@ const PartnerDashboard = () => {
                                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                                 </svg>
                                             </div>
-                                            <p className="text-primary">Loading...</p>
+                                            <p className="text-primary hover:text-white">Loading...</p>
                                         </div>)
                                         : failedTransactions || 0}
                                 </p>
                             </div>
-                            <div className="bg-primary text-white p-4">
+                            <div className="hover:bg-primary hover:text-white px-4 py-2 bg-gray-300 text-primary rounded-lg hover:cursor-pointer">
                                 <p className="text-2xl">Total Amount Transacted 24hrs</p>
                                 <p className="text-[48px] font-semibold">
                                     {" "}
@@ -131,7 +131,7 @@ const PartnerDashboard = () => {
                                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                                 </svg>
                                             </div>
-                                            <p className="text-white">Loading...</p>
+                                            <p className="text-primary hover:text-white">Loading...</p>
                                         </div>
                                     </div>) : totalAmount || 0}
                                 </p>
--- src/pages/partner/sidebar.jsx
+++ src/pages/partner/sidebar.jsx
@@ -1,76 +1,117 @@
 import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { Cpu, HelpCircle, LayoutDashboard, Settings, User } from "lucide-react";
+import { Cpu, HelpCircle, LayoutDashboard, LogOutIcon, User } from "lucide-react";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const Sidebar = () => {
-  const [links, setLinks] = useState([
-    {
-      name: "DASHBOARD",
-      icon: <LayoutDashboard className="h-5 w-5 mr-2 text-2xl" />,
-      href: "/partner-dashboard",
-      active: false,
-    },
-    {
-      name: "PROFILE",
-      icon: <User className="h-5 w-5 mr-2" />,
-      href: "/partner-dashboard/profile",
-      active: false,
-    },
-    {
-      name: "RESOLUTION CENTER",
-      icon: <HelpCircle className="h-5 w-5 mr-2" />,
-      href: "/resolution-center",
-      active: false,
-    },
-    {
-      name: "DEV CENTER",
-      icon: <Cpu className="h-5 w-5 mr-2" />,
-      href: "/partner-dashboard/devcenter",
-      active: false,
-    },
-  ]);
+    const [links, setLinks] = useState([
+        {
+            name: "DASHBOARD",
+            icon: <LayoutDashboard className="h-5 w-5 mr-2 text-2xl" />,
+            href: "/partner-dashboard",
+            active: false,
+        },
+        {
+            name: "PROFILE",
+            icon: <User className="h-5 w-5 mr-2" />,
+            href: "/partner-dashboard/profile",
+            active: false,
+        },
+        {
+            name: "RESOLUTION CENTER",
+            icon: <HelpCircle className="h-5 w-5 mr-2" />,
+            href: "/resolution-center",
+            active: false,
+        },
+        {
+            name: "DEV CENTER",
+            icon: <Cpu className="h-5 w-5 mr-2" />,
+            href: "/partner-dashboard/devcenter",
+            active: false,
+        },
+    ]);
 
-  const location = useLocation();
+    const location = useLocation();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    // Update the links based on the current location
-    setLinks((prevLinks) =>
-      prevLinks.map((link) => ({
-        ...link,
-        active: location.pathname === link.href,
-      }))
+    useEffect(() => {
+        // Update the links based on the current location
+        setLinks((prevLinks) =>
+            prevLinks.map((link) => ({
+                ...link,
+                active: location.pathname === link.href,
+            }))
+        );
+    }, [location.pathname]);
+
+    const handleLogout = async () => {
+        try {
+            // Retrieve the authentication token from local storage
+            const authToken = localStorage.getItem("token");
+
+            // Send a POST request to the server's logout endpoint with the authentication token as data
+            const response = await axios.post(
+                `${BASE_URL}auth/logout`,
+                {
+                    token: authToken,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem("token")}`,
+                    },
+                }
+            );
+
+            // Check if the logout was successful
+            if (response.status === 200) {
+                toast.success("Logout Successful");
+            } else {
+                toast.error("Logout Failed. Please try again.");
+            }
+        } catch (error) {
+            console.log(error);
+            console.error("Error logging out:", error.message);
+            toast.error("An error occurred while logging out. Please try again.");
+        } finally {
+            // Clear authentication token and user email from local storage
+            localStorage.removeItem("token");
+            localStorage.removeItem("userEmail");
+            // Redirect to the login page
+            navigate("/");
+        }
+    };
+
+    return (
+        <div className="min-h-screen flex">
+            <div className="w-full md:w-[372px] h-full overflow-hidden border-r border-body1 flex flex-col justify-between">
+                <div className="flex flex-col pt-8 space-y-4 px-4 md:px-8 lg:px-10 xl:px-12">
+                    {links.map((link) => (
+                        <a
+                            href={link.href}
+                            className={`flex items-center hover:text-primary ${link.active ? "text-primary" : "text-black"
+                                }`}
+                            key={link.name}
+                        >
+                            {link.icon}
+                            <p className="text-lg md:text-2xl">{link.name}</p>
+                        </a>
+                    ))}
+                </div>
+                <div className="px-4 md:px-8 lg:px-10 xl:px-12 pb-8">
+                    <button
+                        onClick={handleLogout}
+                        className={`flex items-center text-black gap-2 px-3 py-2`}
+                    >
+                        <LogOutIcon />
+                        <p className="">LOGOUT</p>
+                    </button>
+                </div>
+            </div>
+        </div>
     );
-  }, [location.pathname]);
-
-  return (
-    <div className="min-h-screen flex">
-      <div className="w-full md:w-[372px] h-full overflow-hidden border-r border-body1 flex flex-col justify-between">
-        <div className="flex flex-col pt-8 space-y-4 px-4 md:px-8 lg:px-10 xl:px-12">
-          {links.map((link) => (
-            <a
-              href={link.href}
-              className={`flex items-center hover:text-primary ${
-                link.active ? "text-primary" : "text-black"
-              }`}
-              key={link.name}
-            >
-              {link.icon}
-              <p className="text-lg md:text-2xl">{link.name}</p>
-            </a>
-          ))}
-        </div>
-        <div className="px-4 md:px-8 lg:px-10 xl:px-12 pb-8">
-          <a
-            href="/settings"
-            className={`flex items-center text-black hover:text-primary`}
-          >
-            <Settings className="mr-2 h-5 w-5" />
-            <p className="text-lg md:text-2xl">SETTINGS</p>
-          </a>
-        </div>
-      </div>
-    </div>
-  );
 };
 
 
