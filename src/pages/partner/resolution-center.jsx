28178621a27d48b67d841202988a6e3672f32a4a
aderounmu <aderounmubamgbose@yahoo.com>
2023-12-14 16:25:57 +0100

    Spelling Errors In Support Page Changed

--- src/pages/partner/resolution-center.jsx
+++ src/pages/partner/resolution-center.jsx
@@ -12,7 +12,7 @@ import { SupportTicketTable } from "../../components/SupportTicketTable/table";
 const ResolutionCenter = () => {
   const { email, isUserDataLoading , entityId } = useUserData(BASE_URL);
   
-  const {ModalProvider: AddComplaintModal, openModal : openAddModal , closeModal} = useModal("Filing A New Ticket")
+  const {ModalProvider: AddComplaintModal, openModal : openAddModal , closeModal} = useModal("New Support Request")
   // get Complaints 
 
 
@@ -70,7 +70,7 @@ const ResolutionCenter = () => {
             </p>
           </div>
           <button onClick={() => openAddModal()}  type="button" class="px-3 flex gap-x-2 items-center py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
-            <Plus/> Make Support Request
+            <Plus/> Make A Support Request
           </button>
           </div>
           <div className="mt-7">
@@ -148,6 +148,21 @@ const AddSupportTicketForm = ({email , entityId , createComplaintMutation , clos
       
     }
 
+    const ticketTypes = [
+      "Payment Issue",
+      "Billing Inquiry",
+      "Account Access",
+      "Service Disruption",
+      "Refund Request",
+      "Transaction Dispute",
+      "Technical Support",
+      "General Inquiry",
+      "Feedback or Suggestions",
+      "Change of Details",
+      "Complaints",
+      "Suspicious Activity",
+    ];
+
   return (
     
     <form>
@@ -165,9 +180,7 @@ const AddSupportTicketForm = ({email , entityId , createComplaintMutation , clos
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5       "
         >
           <option selected>Choose a complaint</option>
-          <option value="Customer Related">Customer Related</option>
-          <option value="Pending Token">Pending Token</option>
-          <option value="Developer Support">Developer Support</option>
+          {ticketTypes?.map(ele =><option value={ele}>{ele}</option>)}
         </select>
       </div>
       
@@ -268,7 +281,7 @@ const AddSupportTicketForm = ({email , entityId , createComplaintMutation , clos
           <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB"/>
           <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor"/>
           </svg>
-          Loading...</> :'Submit Complaint'}
+          Loading...</> :'Submit Request'}
       </button>
     </div>
   </form>
