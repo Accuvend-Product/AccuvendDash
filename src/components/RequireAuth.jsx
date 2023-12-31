10a66ce0df15135c3bfe6e512d71ec7ad4443580
aderounmu <49361042+aderounmu@users.noreply.github.com>
2023-12-31 20:25:44 +0100

    Removed console.log items to terminal

---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,36 @@
+import React , {useEffect , useState} from 'react'
+import { useNavigate , Navigate } from "react-router-dom";
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+import { useLogout } from '../hooks/utilityHooks';
+import useUserData from '../hooks/useUserData';
+import { toast } from "react-hot-toast";
+
+const RequireAuth = ({children}) => {
+    const {handleLogout} = useLogout()
+    const navigate = useNavigate();
+    const { isUserDataError} = useUserData(BASE_URL)
+    useEffect(()=>{
+        const authToken = localStorage.getItem("token");
+        
+        if(!authToken){
+            toast.error("Sorry you have to login to see the dashboard");
+            // Clear authentication token and user email from local storage
+            localStorage.removeItem("token");
+            localStorage.removeItem("userEmail");
+            // Redirect to the login page
+            navigate("/");
+            
+        }else{
+            if(isUserDataError?.response?.data?.message === "Invalid authentication"){
+                handleLogout()
+            }
+        }
+        
+    },[isUserDataError])
+
+
+
+  return children
+}
+
+export default RequireAuth
